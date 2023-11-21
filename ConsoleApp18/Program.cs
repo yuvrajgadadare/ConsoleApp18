1d24e4f62bf42a5bf77f33566bc8fa041a8403db
yuvrajgadadare <42262087+yuvrajgadadare@users.noreply.github.com>
2023-11-21 14:29:09 +0530

    Add project files.

---
+++ ConsoleApp18/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpProjectForGit
+{
+    internal class Program
+    {
+        public void Display()
+        {
+            int a, b, c;
+            c = a + b
+            Console.WriteLine("Welcome");
+        }
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.Display();
+            Console.ReadLine();
+        }
+    }
+}
